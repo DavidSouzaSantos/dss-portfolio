538d0ddb367312740a5a8dc7b86bc63305a86ab2
David Souza dos Santos <david.xvc93@gmail.com>
2021-03-03 13:33:24 -0300

    criado header e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      HeaderComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule
   ],
